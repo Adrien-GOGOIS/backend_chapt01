1cc3a88696b518907363f3e8d4201fc35d321706
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-07 14:22:11 +0100

    Exo2 display authors with ID in URL

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
+const authors = require("./authors");
+
 app.get("/", (req, res, _next) => {
   res.send("Authors API");
+  console.log(authors.authors[0].name);
+});
+
+app.get("/authors/:authorId", (req, res) => {
+  const author = authors.authors[req.params.authorId].name;
+
+  const nationality = authors.authors[req.params.authorId].nationality;
+
+  if (!author) {
+    return res.json({
+      message: "Sorry, this user does not exist!",
+    });
+  }
+
+  res.json(author + ", " + nationality); // On répond toujours à la fin, pas de code après
 });
 
 app.listen(8000, () => {
