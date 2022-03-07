ab99d0172f917f6aa9cfb82f706f0e20460bad67
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-07 14:30:21 +0100

    Display books for each author

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
+// import tableau d'auteurs
 const authors = require("./authors");
 
+// Page d'accueil
 app.get("/", (req, res, _next) => {
   res.send("Authors API");
   console.log(authors.authors[0].name);
 });
 
+// Route pour affichage de l'auteur
 app.get("/authors/:authorId", (req, res) => {
   const author = authors.authors[req.params.authorId].name;
-
   const nationality = authors.authors[req.params.authorId].nationality;
 
   if (!author) {
@@ -20,9 +22,23 @@ app.get("/authors/:authorId", (req, res) => {
     });
   }
 
-  res.json(author + ", " + nationality); // On répond toujours à la fin, pas de code après
+  res.json(author + ", " + nationality);
 });
 
+// Route pour affichage du bouquin
+app.get("/authors/:authorId/books", (req, res) => {
+  const book = authors.authors[req.params.authorId].books;
+
+  if (!book) {
+    return res.json({
+      message: "Sorry, this user does not exist!",
+    });
+  }
+
+  res.json(book.join(", "));
+});
+
+// démarrage serveur
 app.listen(8000, () => {
   console.log("Listening on port 8000");
 });
